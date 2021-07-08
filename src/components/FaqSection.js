a5aadb1faaa4b31fdc003e3824cc6fa03669b27d
DylandeBruijn <dylandebruyn1993@gmail.com>
2021-07-09 01:36:02 +0200

    Faq section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+export const FaqSection = () => {
+	return (
+		<div className="faq">
+			<h2>
+				Vragen? <span>FAQ</span>
+			</h2>
+			<div className="question">
+				<h4>Vraag 1</h4>
+				<div className="answer">
+					<p>Lorem ipsum dolor sit amet.</p>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
+						ipsam.
+					</p>
+				</div>
+			</div>
+			<div className="question">
+				<h4>Vraag 2</h4>
+				<div className="answer">
+					<p>Lorem ipsum dolor sit amet.</p>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
+						ipsam.
+					</p>
+				</div>
+			</div>
+			<div className="question">
+				<h4>Vraag 3</h4>
+				<div className="answer">
+					<p>Lorem ipsum dolor sit amet.</p>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
+						ipsam.
+					</p>
+				</div>
+			</div>
+			<div className="question">
+				<h4>Vraag 4</h4>
+				<div className="answer">
+					<p>Lorem ipsum dolor sit amet.</p>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
+						ipsam.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
