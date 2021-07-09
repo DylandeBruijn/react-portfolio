57616bb41cf8b4a8967111408c90c87ab5f0d31a
DylandeBruijn <dylandebruyn1993@gmail.com>
2021-07-09 12:16:06 +0200

    Styled ServicesSection

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,12 +3,15 @@ import React from "react";
 // Images
 import home1 from "../img/home1.png";
 
-//Styled
+// Styled Components
 import styled from "styled-components";
+
+// Import StyledComponents
+import { StyledSection, StyledDescription, StyledImage, Hide } from "../styles";
 
 export const AboutSection = () => {
 	return (
-		<StyledAbout>
+		<StyledSection>
 			<StyledDescription>
 				<div className="title">
 					<Hide>
@@ -32,38 +35,6 @@ export const AboutSection = () => {
 			<StyledImage>
 				<img src={home1} alt="guy with a camera" />
 			</StyledImage>
-		</StyledAbout>
+		</StyledSection>
 	);
 };
-
-// Styled Component
-const StyledAbout = styled.div`
-	min-height: 90vh;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	padding: 5rem 10rem;
-	color: white;
-`;
-
-const StyledDescription = styled.div`
-	flex: 1;
-	padding-right: 5rem;
-	h2 {
-		font-weight: lighter;
-	}
-`;
-
-const StyledImage = styled.div`
-	flex: 1;
-	overflow: hidden;
-	img {
-		width: 100%;
-		height: 80vh;
-		object-fit: cover;
-	}
-`;
-
-const Hide = styled.div`
-	overflow: hidden;
-`;
--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -1,53 +1,87 @@
 import React from "react";
 
-//Import Icons
+// Import Icons
 import clock from "../img/clock.svg";
 import diaphragm from "../img/diaphragm.svg";
 import money from "../img/money.svg";
 import teamwork from "../img/teamwork.svg";
 
-//Import Images
+// Import Images
 import home2 from "../img/home2.png";
+
+// Import Styled Component
+import styled from "styled-components";
+
+// Import StyledComponents
+import { StyledSection, StyledDescription, StyledImage } from "../styles";
 
 export const ServicesSection = () => {
 	return (
-		<div className="services">
-			<div className="description">
-				<h2>
-					Hoge <span>kwaliteit</span> diensten
-				</h2>
-				<div className="cards">
-					<div className="icon">
-						<img src={clock} alt="icon" />
-						<h3>USP 1</h3>
-					</div>
-					<p>Lorem ipsum dolor sit amet.</p>
-				</div>
-				<div className="cards">
-					<div className="icon">
-						<img src={teamwork} alt="icon" />
-						<h3>USP 2</h3>
-					</div>
-					<p>Lorem ipsum dolor sit amet.</p>
-				</div>
-				<div className="cards">
-					<div className="icon">
-						<img src={diaphragm} alt="icon" />
-						<h3>USP 3</h3>
-					</div>
-					<p>Lorem ipsum dolor sit amet.</p>
-				</div>
-				<div className="cards">
-					<div className="icon">
-						<img src={money} alt="icon" />
-						<h3>USP 4</h3>
-					</div>
-					<p>Lorem ipsum dolor sit amet.</p>
-				</div>
-			</div>
-			<div className="image">
+		<StyledServices>
+			<StyledDescription>
+				<StyledCards>
+					<StyledCard>
+						<div className="icon">
+							<img src={clock} alt="icon" />
+							<h3>USP 1</h3>
+						</div>
+						<p>Lorem ipsum dolor sit amet.</p>
+					</StyledCard>
+					<StyledCard>
+						<div className="icon">
+							<img src={teamwork} alt="icon" />
+							<h3>USP 2</h3>
+						</div>
+						<p>Lorem ipsum dolor sit amet.</p>
+					</StyledCard>
+					<StyledCard>
+						<div className="icon">
+							<img src={diaphragm} alt="icon" />
+							<h3>USP 3</h3>
+						</div>
+						<p>Lorem ipsum dolor sit amet.</p>
+					</StyledCard>
+					<StyledCard>
+						<div className="icon">
+							<img src={money} alt="icon" />
+							<h3>USP 4</h3>
+						</div>
+						<p>Lorem ipsum dolor sit amet.</p>
+					</StyledCard>
+				</StyledCards>
+			</StyledDescription>
+			<StyledImage>
 				<img src={home2} alt="camera" />
-			</div>
-		</div>
+			</StyledImage>
+		</StyledServices>
 	);
 };
+
+const StyledServices = styled(StyledSection)`
+	h2 {
+		padding-bottom: 5rem;
+	}
+	p {
+		width: 70%;
+		padding: 2rem 0rem 4rem 0rem;
+	}
+`;
+
+const StyledCards = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+`;
+
+const StyledCard = styled.div`
+	flex-basis: 20rem;
+	.icon {
+		display: flex;
+		align-items: center;
+		h3 {
+			margin-left: 1rem;
+			background: white;
+			color: black;
+			padding: 1rem;
+		}
+	}
+`;
