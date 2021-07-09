5dc4299b44583784751d07795bcbcf4c1c4cc75a
DylandeBruijn <dylandebruyn1993@gmail.com>
2021-07-09 11:41:15 +0200

    Style AboutSection.js

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,32 +3,67 @@ import React from "react";
 // Images
 import home1 from "../img/home1.png";
 
+//Styled
+import styled from "styled-components";
+
 export const AboutSection = () => {
 	return (
-		<div>
-			<div className="description">
+		<StyledAbout>
+			<StyledDescription>
 				<div className="title">
-					<div className="hide">
+					<Hide>
 						<h2>Creatieve duizendpoot</h2>
-					</div>
-					<div className="hide">
+					</Hide>
+					<Hide>
 						<h2>
 							met <span>passie</span> voor
 						</h2>
-					</div>
-					<div className="hide">
+					</Hide>
+					<Hide>
 						<h2>de digitale wereld</h2>
-					</div>
+					</Hide>
 				</div>
 				<p>
 					Neem contact op om te informeren wat ik voor u of uw bedrijf kan
 					betekenen!
 				</p>
 				<button>Contact</button>
-			</div>
-			<div className="image">
+			</StyledDescription>
+			<StyledImage>
 				<img src={home1} alt="guy with a camera" />
-			</div>
-		</div>
+			</StyledImage>
+		</StyledAbout>
 	);
 };
+
+// Styled Component
+const StyledAbout = styled.div`
+	min-height: 90vh;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 5rem 10rem;
+	color: white;
+`;
+
+const StyledDescription = styled.div`
+	flex: 1;
+	padding-right: 5rem;
+	h2 {
+		font-weight: lighter;
+	}
+`;
+
+const StyledImage = styled.div`
+	flex: 1;
+	overflow: hidden;
+	img {
+		width: 100%;
+		height: 80vh;
+		object-fit: cover;
+	}
+`;
+
+const Hide = styled.div`
+	overflow: hidden;
+`;
