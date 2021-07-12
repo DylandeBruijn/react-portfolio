fbb53fcd887741545dfa9990606896795821732a
DylandeBruijn <dylandebruyn1993@gmail.com>
2021-07-12 23:36:38 +0200

    Add contact

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -12,6 +12,7 @@ body {
     background: #1b1b1b;
     font-family: 'Inter', sans-serif;
     margin-top: 10vh;
+    overflow-x: hidden;
 }
 
 button {
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,17 +1,72 @@
+// Styled Components
+import styled from "styled-components";
+
 // Animations
 import { motion } from "framer-motion";
-import { pageAnimation } from "../animation";
+import { pageAnimation, titleAnimation } from "../animation";
 
 export const Contact = () => {
 	return (
-		<motion.div
+		<StyledContact
 			variants={pageAnimation}
 			initial="hidden"
 			animate="show"
 			exit="exit"
 			style={{ background: "#fff" }}
 		>
-			<h1>Contact</h1>
-		</motion.div>
+			<StyledTitle>
+				<StyledHide>
+					<motion.h2 variants={titleAnimation}>Contact</motion.h2>
+				</StyledHide>
+			</StyledTitle>
+			<StyledHide>
+				<StyledSocial variants={titleAnimation}>
+					<StyledCircle />
+					<h2>Send Me A Message</h2>
+				</StyledSocial>
+			</StyledHide>
+			<StyledHide>
+				<StyledSocial variants={titleAnimation}>
+					<StyledCircle />
+					<h2>Send An Email</h2>
+				</StyledSocial>
+			</StyledHide>
+			<StyledHide>
+				<StyledSocial variants={titleAnimation}>
+					<StyledCircle />
+					<h2>Social Media</h2>
+				</StyledSocial>
+			</StyledHide>
+		</StyledContact>
 	);
 };
+
+const StyledContact = styled(motion.div)`
+	padding: 5rem 10rem;
+	color: #353535;
+	min-height: 90vh;
+`;
+
+const StyledTitle = styled.div`
+	margin-bottom: 4rem;
+	color: black;
+`;
+
+const StyledHide = styled.div`
+	overflow: hidden;
+`;
+
+const StyledCircle = styled.div`
+	border-radius: 50%;
+	width: 3rem;
+	height: 3rem;
+	background: #353535;
+`;
+
+const StyledSocial = styled(motion.div)`
+	display: flex;
+	align-items: center;
+	h2 {
+		margin: 2rem;
+	}
+`;
